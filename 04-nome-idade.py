5a25a899c1a3230f9a6616f23ba3993f92adecef
Stephanie de Araújo <stephanie.araujo.farias@gmail.com>
2024-08-29 11:52:58 -0300

    Add files via upload
    29.08.24
    aula 04
    - Para conseguir escrever 
    - input

---
+++ 04-nome-idade.py
@@ -0,0 +1,16 @@
+# Atribuição de variáveis 
+nome = "Stephanie"
+idade = 16
+altura = 1.69
+
+# Exibir mensagem de tela
+print("nome:", nome, " idade:", idade, " altura:", altura)
+# Formatação com f-string
+print(f"nome:{nome} idade:{idade} altura:{altura}")
+
+# "Input" é como o leia no Portugol
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura: "))
+
+print(f"nome:{nome} idade:{idade} altura:{altura}")
