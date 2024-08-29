bd91c1d93d398ec6c7eabc50fe323c9f4c8ff7d8
Stephanie de Araújo <stephanie.araujo.farias@gmail.com>
2024-08-29 11:51:28 -0300

    Add files via upload
    29.08.24
    Aula 03
    Falando sobre variáveis

---
+++ 03-aula03.py
@@ -0,0 +1,19 @@
+# Variáveis em python
+# Não há necessidade de declará-las no início do programa
+# Basta criar no meio do algoritmo com a atribuição 
+
+teste = "Stephanie"
+print(teste) # Imprime a string "Stephanie"
+print (type(teste)) # Imprime o tipo da variável, string
+
+teste = 10
+print(teste) # Imprime o número 10
+print (type(teste)) # Imprime o tipo da variável, que é int
+
+teste = 1.74
+print(teste) # Imprime o número 1.74
+print (type(teste)) # Imprime o tipo da variável, que é float
+
+teste = True
+print(teste) # Imprime o valor True
+print (type(teste)) # Imprime o tipo da variável, que é bool
