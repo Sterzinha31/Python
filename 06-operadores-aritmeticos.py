b543700b25a097650645db8b5cead9f1fe410c08
Stephanie de Araújo <stephanie.araujo.farias@gmail.com>
2024-09-10 11:14:14 -0300

    Add files via upload
    10.09.24

---
+++ 06-operadores-aritmeticos.py
@@ -0,0 +1,29 @@
+# Operadores Aritméticos
+
+# Soma
+soma = 10 + 5
+print(soma)
+
+# Subtração
+subtracao = 10 - 5
+print(subtracao)
+
+# Multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+
+# Divisão
+divisao = 10 / 5
+print(divisao)
+
+# Divisão INTEIRA
+divisao_inteira = 10 // 5
+print(divisao_inteira)
+
+# Exponenciação 
+exponenciacao = 10 ** 5
+print(exponenciacao)
+
+# Módulo / Resto da Divisão
+modulo = 10 % 5
+print(modulo)
