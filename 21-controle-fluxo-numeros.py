13c871b96e560c2f0af6fc7b7c99c887d3c11bb5
Stephanie de Araújo <stephanie.araujo.farias@gmail.com>
2024-10-16 11:11:55 -0300

    Add files via upload

---
+++ 21-controle-fluxo-numeros.py
@@ -0,0 +1,19 @@
+numero = 0
+
+while numero <= 100:
+    numero += 1
+
+    if numero == 6:
+      print("Não vou mostrar o 6!")
+      continue
+
+    if  numero >= 10 and numero <= 27:
+        print(f"Não vou mostrar o {numero}")
+        continue
+
+    print(numero)
+
+    if numero == 40:
+      break
+    
+print("Acabou!")
